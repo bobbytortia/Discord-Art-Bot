870150653800050d1bcd222bea458c526d466ebf
s3842025 <s3842025@student.rmit.edu.au>
2025-05-07 20:13:16 +1000

    commit

--- keepAlive.js
+++ keepAlive.js
@@ -2,5 +2,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Bot is running.'));
-app.listen(3000, () => console.log('KeepAlive server started'));
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Web server listening on port ${PORT}`);
+});
